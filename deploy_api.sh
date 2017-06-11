e0d298152b4016b12334f7ffea5f835479e35c10
supperbowen <bowenliang@outlook.com>
2017-06-11 10:54:07 +0800

    update deploy

--- deploy_api.sh
+++ deploy_api.sh
@@ -8,10 +8,13 @@ cp -r app/ /usr/local/source/crm/api
 rm -rf api.zip
 rm -rf app
 
-cp package.json /usr/local/source/crm
+#cp package.json /usr/local/source/crm
 cd /usr/local/source/crm
+rm -rf bowen-crm-api
+git clone https://github.com/supperbowen/bowen-crm-api.git
+cd bowen-crm-api
 cnpm install
-nohup node api
+nohup npm run prod
 
 
 
