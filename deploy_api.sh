9caf5df35b1fa9d6eb18438d9840ab55a5dd33a4
supperbowen <bowenliang@outlook.com>
2017-06-11 13:29:17 +0800

    test

--- deploy_api.sh
+++ deploy_api.sh
@@ -5,7 +5,7 @@ rm -rf bowen-crm-api
 git clone https://github.com/supperbowen/bowen-crm-api.git
 cd bowen-crm-api
 cnpm install
-sh /usr/local/source/kill-node.sh
-nohup npm run prod & exit 1
+sh /usr/local/source/kill-node.sh 
+nohup npm run prod &
 
 
