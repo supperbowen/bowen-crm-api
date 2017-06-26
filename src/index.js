7de68bad6a71f4f319fe7941113040747b733c51
bowen <liangcanl@126.com>
2017-06-26 23:27:46 +0800

    remove oauth we will move the oauth to a separate service

--- src/index.js
+++ src/index.js
@@ -3,24 +3,9 @@ import {
 	koaRouter
 } from './router';
 import context from './common/context';
-//import context from './common/context';
-
-const bodypraser = require('koa-bodyparser');
-const mount = require('koa-mount');
-const oauthserver = require('koa-oauth-server');
-const model = require('./models/oauth.schema');
 
 const Koa = require('koa');
-//const mongoose = require('mongoose');
 const app = new Koa();
-
-app.oauth = oauthserver({
-	model: model, // See https://github.com/thomseddon/node-oauth2-server for specification
-	grants: ['password'],
-	debug: true
-});
-
-app.use(bodypraser());
 
 //import controllers to app, we should do this or our controller will not execute!
 //把要使用到的controller引到当前app中，我们必须这样做，否则controller被不会被加载运行。
@@ -28,18 +13,11 @@ import controllers from './controllers';
 controllers.bindRouters();
 
 
-// Mount `oauth2` route prefix.
-app.use(mount('/oauth2', koaRouter.middleware()));
-
-// Register `/token` POST path on oauth router (i.e. `/oauth2/token`).
-koaRouter.post('/token', app.oauth.grant());
-
-
 app.use(koaRouter.routes())
 	.use(koaRouter.allowedMethods());
 
 var mongoose = require('mongoose');
-mongoose.connect(context.dburl);
+mongoose.openUri(context.dburl);
 
 app.listen(8088);
 console.log('server started : http://localhost:8088/');
